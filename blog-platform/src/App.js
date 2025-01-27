45c43fe37948a9452f03c694c7e90e4d2c291ec8
Shital <shitalghodake2@gmail.com>
2025-01-27 17:38:08 +0530

    Created new component for POST Creation

--- blog-platform/src/App.js
+++ blog-platform/src/App.js
@@ -1,13 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-import About from './components/About';
+//import logo from './logo.svg';
+//import './App.css';
+import Post from './components/pages/Post';
+//import About from './components/About';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import Card from './components/practicePost';
 
-function App() {
+
+//import { BrowserRouter, Link } from "react-router-dom";
+//import CreatePost from './components/pages/Post';
+
+// function App() {
+//   return (
+//     <>
+//     <CreatePost/>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+  function App() {
+    return (
+       <>
+       <Post/>
+</>
+     );
+   }
+  
+   export default App;
+  
+  /* import React from 'react';
+import Card from './Card';*/
+
+/*function App() {
   return (
-    <div>
-      <About/>
-      </div>
+    <div className="App">
+      <h1>My Bootstrap Card Example</h1>
+      <Card />
+    </div>
   );
 }
 
 export default App;
+*/
